9d466a4665148464144e9c9f189e3a7358247223
Your Name <you@example.com>
2025-03-29 13:59:50 -0500

    files templates created

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,25 @@
+class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def search
+  end
+
+  def vote
+  end
+end
---
+++ test/controllers/restaurants_controller_test.rb
@@ -0,0 +1,43 @@
+require "test_helper"
+
+class RestaurantsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get restaurants_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get restaurants_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get restaurants_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get restaurants_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get restaurants_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get restaurants_update_url
+    assert_response :success
+  end
+
+  test "should get search" do
+    get restaurants_search_url
+    assert_response :success
+  end
+
+  test "should get vote" do
+    get restaurants_vote_url
+    assert_response :success
+  end
+end
