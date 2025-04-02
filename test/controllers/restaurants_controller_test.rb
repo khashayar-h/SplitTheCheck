227e9e2714ad27f7d620d349bad5a832831de36f
Your Name <you@example.com>
2025-04-02 15:34:55 -0500

    Added tests for all controllers

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,43 +1,62 @@
 require "test_helper"
 
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @restaurant = Restaurant.create(name: "Test Place", location: "New York", will_split_votes: 5, wont_split_votes: 2)
+  end
+
   test "should get index" do
-    get restaurants_index_url
+    get restaurants_url
     assert_response :success
+    assert_select "h1", "Split The Check"
   end
 
-  test "should get show" do
-    get restaurants_show_url
+  test "should show restaurant" do
+    get restaurant_url(@restaurant)
     assert_response :success
+    assert_select "h1", @restaurant.name
   end
 
-  test "should get new" do
-    get restaurants_new_url
+  test "should get new restaurant form" do
+    get new_restaurant_url
     assert_response :success
+    assert_select "form"
   end
-
-  test "should get create" do
-    get restaurants_create_url
-    assert_response :success
+  test "should create restaurant and redirect to index" do
+    assert_difference('Restaurant.count', 1) do
+      post restaurants_url, params: { restaurant: { name: "Test Restaurant", location: "Test Location", will_split: 1, wont_split: 0 } }
+    end
+      assert_redirected_to restaurants_path
   end
+  
 
   test "should get edit" do
-    get restaurants_edit_url
+    get edit_restaurant_url(@restaurant)
     assert_response :success
+    assert_select "form"
   end
 
-  test "should get update" do
-    get restaurants_update_url
-    assert_response :success
+  test "should update restaurant" do
+    patch restaurant_url(@restaurant), params: { restaurant: { name: "Updated Name" } }
+    assert_redirected_to restaurant_url(@restaurant)
+    @restaurant.reload
+    assert_equal "Updated Name", @restaurant.name
   end
 
-  test "should get search" do
-    get restaurants_search_url
-    assert_response :success
+  test "should search for a restaurant" do
+    get search_restaurants_url, params: { query: "Test" } if defined?(search_restaurants_url)
+    assert_response :success if defined?(search_restaurants_url)
   end
 
-  test "should get vote" do
-    get restaurants_vote_url
-    assert_response :success
+  test "should vote for a restaurant" do
+    restaurant = restaurants(:one)
+    initial_will_split_votes = restaurant.will_split_votes
+  
+    post vote_restaurant_url(restaurant, vote: "will_split")
+  
+    restaurant.reload
+  
+    assert_equal initial_will_split_votes + 1, restaurant.will_split_votes
   end
+  
 end
